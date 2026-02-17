34a45102f791cd5304e4cd93bd328e1daf3efff9
sakshit1441 <109957625+sakshit1441@users.noreply.github.com>
2026-02-18 00:53:04 +0530

    Change S3 backend region from us-east-1 to ap-south-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "oneclick-mongodb"
     key     = "aws_terraform/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "ap-south-1"
     encrypt = true
   }
 }
