00745d645b12154d6442c609bcae4c88ce98b8e7
sakshit1441 <109957625+sakshit1441@users.noreply.github.com>
2026-02-24 03:14:34 +0530

    Update outputs.tf

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,16 +1,31 @@
+##########################
+# ALB Outputs
+##########################
+
+# Target group ARN
 output "tg_arn" {
-  value = aws_lb_target_group.tg.arn
+  description = "ARN of the target group attached to the ALB"
+  value       = aws_lb_target_group.tg.arn
 }
 
+# ALB DNS name
 output "alb_dns" {
-  value = aws_lb.this.dns_name
+  description = "DNS name of the ALB"
+  value       = aws_lb.this.dns_name
 }
 
+# ✅ FIX: Handle conditional SG creation (works even if existing SG is reused)
 output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id
+  description = "Security group ID for the ALB"
+  value = (
+    data.aws_security_group.existing_alb_sg.id != "" ?
+    data.aws_security_group.existing_alb_sg.id :
+    aws_security_group.alb_sg[0].id
+  )
 }
 
+# ALB ARN
 output "alb_arn" {
-  value = aws_lb.this.arn
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.this.arn
 }
-
